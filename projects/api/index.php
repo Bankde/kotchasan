82775b190c40f283f3269f3d545cee6f6b8f24cc
goragodwiriya <2264492+goragod@users.noreply.github.com>
2023-06-23 21:48:19 +0700

    update 5.0.0 PHP8.2

---
+++ projects/api/index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @filesource projects/api/index.php
+ *
+ * Main entry point for the API project.
+ * This file initializes the Kotchasan Framework and starts the web application.
+ * For more information, please visit: https://www.kotchasan.com/
+ *
+ * @copyright 2016 Goragod.com
+ * @license https://www.kotchasan.com/license/
+ * @author Goragod Wiriya <admin@goragod.com>
+ * @package Kotchasan
+ */
+
+/**
+ * Debug mode:
+ * 0 (default): Log severe errors to error_log.php.
+ * 1: Log errors and warnings to error_log.php.
+ * 2: Display errors and warnings on the screen (only for development purposes).
+ */
+define('DEBUG', 2);
+
+/**
+ * Database query logging:
+ * false (default): Do not log database queries.
+ * true: Log database queries to log file (only for development purposes).
+ */
+define('DB_LOG', false);
+
+// Load Kotchasan Framework
+include '../../Kotchasan/load.php';
+
+// Initialize Kotchasan Framework and start the web application
+Kotchasan::createWebApplication()->run();
